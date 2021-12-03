cc2ddf14f73a7e62dc7779aae66e881ad87bbffe
JazminSC <l18390058@chetumal.tecnm.mx>
2021-12-03 08:59:23 -0500

    SI
    Signed-off-by: JazminSC <l18390058@chetumal.tecnm.mx>

--- MatrizSuperior.js
+++ MatrizSuperior.js
@@ -1,23 +1,31 @@
-const Aceptar = function MatrizSuperior(){
+const Aceptar = function MatrizSuperior()
+{
     
     var columna= document.getElementById("Columna").value;
     var fila = document.getElementById("Fila").value;
     
-    if (columna==fila){
+    if (columna==fila)
+    {
         console.log();
         document.getElementById("resultado").innerHTML =  "Esta matriz es triangular superiror"+ "<br>"+ "<br>";
-    }else{
+    }else
+    {
         document.getElementById("resultado").innerHTML =  "Esta matriz no es triangular superiror" + "<br>"+ "<br>";
     }
 
     let matriz =[];
-    for (let i =0; i < columna; ++i){
+    for (let i =0; i < columna; ++i)
+    {
         let filas = [];
-        for (let j =0; j < fila; ++j){
-            if(i>j){
+        for (let j =0; j < fila; ++j)
+        {
+            if(i>j)
+            {
                 filas.push(0);
 
-            }else{
+            }
+            else
+            {
                 
 
                 filas.push (Math.floor(Math.random()*9+1));
